5261f4f79d7b08a23537a12c80179874c51b4c0e
MoSherman <moriahsherman@gmail.com>
2015-08-30 12:12:39 +0100

    adding script

---
+++ P0017_morefiles/morefiles.py
@@ -0,0 +1,30 @@
+#Based on exercise 17 of learn python the hard way. 
+
+#START 
+
+from sys import argv 
+from os.path import exists 
+
+script, from_file, to_file = argv 
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+#Making the following into one line
+in_file = open(from_file);
+indata = in_file.read()
+
+print "The input file is %d bytes long." % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input() 
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done." 
+
+out_file.close()
+in_file.close()
+
+#END
