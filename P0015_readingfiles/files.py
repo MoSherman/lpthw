e572e5cfe82f8ccd6b0fbe0281eba0bfd79fd6f7
MoSherman <moriahsherman@gmail.com>
2015-08-30 10:35:22 +0100

    adding close file

--- P0015_readingfiles/files.py
+++ P0015_readingfiles/files.py
@@ -11,11 +11,17 @@ txt = open(filename)
 print "Here's your file %r:" % filename 
 print txt.read() 
 
+txt.close()
+
 print "Type the filename again:"
 file_again = raw_input('>')
 
 txt_again = open(file_again) 
 
 print txt_again.read()
+
+txt_again.close()
+
+
 
 #END
