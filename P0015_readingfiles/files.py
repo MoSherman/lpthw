f8fc2ef8932a01a1c1db06ed1530221b585782bd
MoSherman <moriahsherman@gmail.com>
2015-08-30 10:30:31 +0100

    adding files.py

---
+++ P0015_readingfiles/files.py
@@ -0,0 +1,21 @@
+#Based on exercise 15 of learn python the hardway 
+
+#START 
+
+from sys import argv 
+
+script, filename = argv 
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename 
+print txt.read() 
+
+print "Type the filename again:"
+file_again = raw_input('>')
+
+txt_again = open(file_again) 
+
+print txt_again.read()
+
+#END
