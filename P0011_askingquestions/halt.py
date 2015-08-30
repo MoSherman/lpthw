f98bd5943ac3e446d791e0fda72beebbf367ce9d
MoSherman <moriahsherman@gmail.com>
2015-08-30 09:00:17 +0100

    adding halt program

---
+++ P0011_askingquestions/halt.py
@@ -0,0 +1,12 @@
+#More practice, taken from stack overflow suggestions. 
+
+#It seems that you can put questions into the raw_input prompt.
+#Here is my attempt at trying this. 
+
+#START 
+
+print "Halt!", 
+name = raw_input ("Who goes there? ") 
+print "Oh its just %r. You may pass %r." % (name) 
+
+#END
