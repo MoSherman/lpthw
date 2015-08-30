d9567a3fb300134ed2289ede2a8c3e77c6abe7b3
MoSherman <moriahsherman@gmail.com>
2015-08-30 09:41:23 +0100

    adding arguments.py

---
+++ P0013_arguments/arguments.py
@@ -0,0 +1,15 @@
+#Based on the thirteenth learn python the hard way exercise.
+#Making scripts that accept arguments. 
+
+#START 
+
+from sys import argv 
+
+script, first, second, third = argv 
+
+print "The script is called:", script 
+print "Your first variable is:", first 
+print "Your second variable is:", second 
+print "Your third variable is:", third
+
+#END
