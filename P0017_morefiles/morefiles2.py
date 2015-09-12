e1ae83544c06c896b9a884d95433667d981dd909
MoSherman <moriahsherman@gmail.com>
2015-09-12 15:03:23 +0100

    fixing syntax problems and spelling

--- P0017_morefiles/morefiles2.py
+++ P0017_morefiles/morefiles2.py
@@ -9,12 +9,12 @@ script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file) 
 
-in_file = open(from_file), 
+in_file = open(from_file)
 indata = in_file.read() 
 
 print "This input file is %d bytes long" % len(indata) 
 
-print "Does the output file exist? %r" % exisits(to_file)
+print "Does the output file exist? %r" % exists(to_file)
 print "Ready? Hit Return to continue." 
 raw_input("?")
 
