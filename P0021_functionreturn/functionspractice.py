608b7c95eff21eabe446b31fd95e0aa83911bed7
Mo Sherman <moriahsherman@gmail.com>
2015-09-18 12:14:20 +0100

    Create functionspractice.py

---
+++ P0021_functionreturn/functionspractice.py
@@ -0,0 +1,15 @@
+#This is my first attempt to code using koding.com
+#This is practice for exercise 21
+
+#START 
+
+def crackers(white, brown):
+    print "We have %r white crackers and %r brown crackers." % (white, brown)
+    return white + brown
+
+total = crackers(10, 20)
+
+print "We have %r crackers in total." % total
+
+
+#END
