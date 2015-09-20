ae15e58272a99176190f8bd2172c805030dbe107
MoSherman <moriahsherman@gmail.com>
2015-09-20 13:18:26 +0100

    fixing mistakes

--- P0024_morepractice/morepractice.py
+++ P0024_morepractice/morepractice.py
@@ -28,10 +28,10 @@ def secret_formula(started):
     return jelly_beans, jars, crates 
     
 start_point = 10000
-beans, jars, crates = secret_formula(star_point)
+beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
-print "We'd have %D beans, %d jars, and %d crates." % (beans, jars, crates)
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
 start_point = start_point / 10
 
