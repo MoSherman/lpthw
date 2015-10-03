d478053e76653188a18709c9f94bdb02013d5d56
MoSherman <moriahsherman@gmail.com>
2015-10-03 12:36:59 +0100

    spelling mistakes

--- P0029_whatif/whatif.py
+++ P0029_whatif/whatif.py
@@ -3,12 +3,12 @@
 
 people = 20 
 cats = 30 
-digs = 15 
+dogs = 15 
 
 if people < cats: 
     print "Too many cats! The world is doomed!"
 
-it people > cats: 
+if people > cats: 
     print "Not many cats! The world is saved!"
 
 if people < dogs: 
