b99d5e6a757abb30f54558cc00a5316249327b7b
MoSherman <moriahsherman@gmail.com>
2015-10-03 12:34:56 +0100

    first commit

---
+++ P0029_whatif/whatif.py
@@ -0,0 +1,32 @@
+#The 29th exercise of lpthw.
+#START 
+
+people = 20 
+cats = 30 
+digs = 15 
+
+if people < cats: 
+    print "Too many cats! The world is doomed!"
+
+it people > cats: 
+    print "Not many cats! The world is saved!"
+
+if people < dogs: 
+    print "The world is drooled on!"
+
+if people > dogs: 
+    print "The world is dry!"
+
+dogs += 5 
+
+if people >= dogs: 
+    print "People are greater than of qual to dogs."
+
+if people <+ dogs: 
+    print "People are less then or equal to dogs."
+
+if people == dogs: 
+    print "People equal dogs."
+
+#END    
+             
