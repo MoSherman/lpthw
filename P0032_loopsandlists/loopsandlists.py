f119d5800d253b4fa393649b7faa3ccfd033822f
MoSherman <moriahsherman@gmail.com>
2015-10-03 13:31:51 +0100

    fixed

--- P0032_loopsandlists/loopsandlists.py
+++ P0032_loopsandlists/loopsandlists.py
@@ -3,7 +3,7 @@
 
 the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
-change = [1, 'pennies', 2, 'dimes, 3, 'quaters']
+change = [1, 'pennies', 2, 'dimes', 3, 'quaters']
 
 #this first kind of for-loop goes through the list 
 for number in the_count:
@@ -15,14 +15,14 @@ for fruit in fruits:
 
 #also we can go through mixed list 
 #notice we have to use %r since we don't know what's in it 
-for i in change 
+for i in change: 
     print "I got %r" % i
 
 #we an also build lists, first start with an empty on
 elements = []
 
 #then use the range function to do 0 to 5 counts 
-for i in range(0,6)
+for i in range(0,6):
     print "Adding %d to the list." % i
     #append is a funtion that lists understand
     elements.append(i)
