4703afda49254429bc9207fe6378309d52f95ffc
MoSherman <moriahsherman@gmail.com>
2015-10-03 12:55:04 +0100

    first commit

---
+++ P0030_elseandif/elseandif.py
@@ -0,0 +1,29 @@
+#Exercise 30 of lpthw.
+#START 
+
+people = 30 
+cars = 40 
+trucks = 15 
+
+if cars > people: 
+    print "We should take cars."
+elif cars < people:
+    print "We should not take the cars."
+else: 
+    print "We can't decide."
+
+if trucks > cars:
+    print "That's too many turcks."
+elif trucks < cars:
+    print "Maybe we could take the trucks."
+else: 
+    print "We still can't decide."
+
+if people > trucks: 
+    print "Alright, let's just take the trucks."
+else: 
+    print "Fine, let's stay at home then."
+    
+#END
+
+    
