d59420a9feebfd7c29b830f8c0956dd31b51e684
MoSherman <moriahsherman@gmail.com>
2015-10-04 11:56:56 +0100

    practice fixed

--- P0033_whileloops/whileloopsfunction.py
+++ P0033_whileloops/whileloopsfunction.py
@@ -2,7 +2,8 @@
 
 #START 
 
-i = raw_input("What is i? ") 
+i_define = raw_input("What is i? ") 
+i = int(i_define)
 numbers = []
 
 def while_loop(i): 
@@ -16,8 +17,14 @@ def while_loop(i):
     
     print "The numbers: "
     
-    for num in numbers 
+    for num in numbers: 
         print num
+
+while_loop(i)
+
+raw_input("Run next loop with i + 2 in function arg line? Hit Enter.")
+
+while_loop(i + 2)
 
 #END
 
