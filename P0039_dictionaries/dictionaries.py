dd656a627a2182a8fd479c7b6093b21442a9dbcd
MoSherman <moriahsherman@gmail.com>
2015-10-17 12:25:24 +0100

    adding ex 39

---
+++ P0039_dictionaries/dictionaries.py
@@ -0,0 +1,76 @@
+#Based on exercise 39 of lpthw
+#START
+
+#create a mapping of state to abbreviation
+states = {
+    'Oregon' : 'OR',
+    'Florida' : 'FL', 
+    'California' : 'CA', 
+    'New York' : 'NY', 
+    'MIchigan' : 'MI'
+}
+
+#create a basice set of states and some cities in them
+cities = {
+    'CA' : 'San Francisco', 
+    'MI' : 'Detroit', 
+    'FL' : 'Jacksonville'
+}
+
+#add more cities 
+cities['NY'] = 'New York City'
+cities['OR'] = 'Portland'
+
+#print out some cities 
+print '_' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
+
+#print some state
+print '_' * 10
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has: ", cities[states['Florida']]
+
+#print every state abbreviation
+print '_' * 10 
+for state, abbrev in states.items():
+    print "%s State is abbreviated %s" (state, abbrev)
+    
+#print every city in state
+print '_' * 10
+for abbrev, city in cities.item():
+    print "%s has the city %s" % (abbrev, city)
+
+#now do both at the same time
+print '_' * 10
+for state, abbrev in states.items():
+    print "%s State is abbreviated %s and has city %s" % (
+        state, abbrev, cities[abbrev])
+
+print '_' * 10
+#safely get a abbreviation by state that might not be there
+states = states.get('Texas')
+
+if not state:
+    print "Sorry no Texas."
+
+#get a city with a default value 
+city = cities.get('TX', 'Does not Exist')
+print "The city for the state 'TX' is: %s" % city 
+
+#END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
