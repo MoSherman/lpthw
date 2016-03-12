83255c3118f2b595df294bbddd6805ecb8aa3f3c
mosherman <moriahsherman@gmail.com>
2016-03-12 12:46:19 +0000

    adding an if elif else

--- P0040_classes/name.py
+++ P0040_classes/name.py
@@ -11,15 +11,38 @@ class Name(object):
         
         print user_name
     
+    def last_name(self):
+        
+        if user_name == 'Moriah':
+            print "I bet your full name is %s Sherman" % user_name
+            print "And your a developer!"
+        
+        elif user_name == 'Sarah':
+            print "I bet your full name is %s Sherman" % user_name
+            print "And your a lawyer!"
+        
+        elif user_name == 'Marc':
+            print "I bet your full name is %s Sherman" % user_name
+            print "And you work in IT!"
+        
+        elif user_name == 'Caroline':
+            print "I bet your full name is %s Sherman" % user_name
+            print "And you are the president of the womens club!"
+        
+        else:
+            print "I bet your full name is %s Sherman" % user_name
+    
 
 user_name = raw_input("Whats your name? > ") 
 
 user = Name(["user_name"])
 
 user.your_name()
+user.last_name()
 
 user_name = raw_input("Whats your name user2? > ")
 
 user2 = Name(["user_name"])
 
 user2.your_name()
+user2.last_name()
