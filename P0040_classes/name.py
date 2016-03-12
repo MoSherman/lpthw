c248ce09df629fe756f47f50fd769cbe0a18ac61
mosherman <moriahsherman@gmail.com>
2016-03-12 12:38:50 +0000

    class practice with raw input

---
+++ P0040_classes/name.py
@@ -0,0 +1,25 @@
+""" 
+A script to allow someone to enter their name.
+"""
+
+class Name(object):
+    
+    def __init__(self, names):
+        self.names = names 
+    
+    def your_name(self):
+        
+        print user_name
+    
+
+user_name = raw_input("Whats your name? > ") 
+
+user = Name(["user_name"])
+
+user.your_name()
+
+user_name = raw_input("Whats your name user2? > ")
+
+user2 = Name(["user_name"])
+
+user2.your_name()
